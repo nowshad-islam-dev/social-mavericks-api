a6f3f5c00abf77bd77e693153cb16795fcf87601
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2026-06-18 18:41:53 +0600

    Add partner content type and related components; update about and service schemas with new attributes

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -11,14 +11,59 @@ export interface ContentFaqItem extends Struct.ComponentSchema {
   };
 }
 
+export interface ContentMilestones extends Struct.ComponentSchema {
+  collectionName: 'components_content_milestones';
+  info: {
+    displayName: 'Milestones';
+  };
+  attributes: {
+    description: Schema.Attribute.Text & Schema.Attribute.Required;
+    title: Schema.Attribute.String & Schema.Attribute.Required;
+    year: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+export interface ContentStats extends Struct.ComponentSchema {
+  collectionName: 'components_content_stats';
+  info: {
+    displayName: 'Stats';
+  };
+  attributes: {
+    description: Schema.Attribute.String & Schema.Attribute.Required;
+    label: Schema.Attribute.String & Schema.Attribute.Required;
+    value: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
 export interface SharedDifferencePoint extends Struct.ComponentSchema {
   collectionName: 'components_shared_difference_points';
   info: {
     displayName: 'Difference point';
   };
   attributes: {
-    description: Schema.Attribute.Text;
-    title: Schema.Attribute.String;
+    description: Schema.Attribute.Text & Schema.Attribute.Required;
+    title: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+export interface SharedFounders extends Struct.ComponentSchema {
+  collectionName: 'components_shared_founders';
+  info: {
+    displayName: 'Founders';
+  };
+  attributes: {
+    bio: Schema.Attribute.Text & Schema.Attribute.Required;
+    email: Schema.Attribute.String & Schema.Attribute.Required;
+    github: Schema.Attribute.String;
+    initials: Schema.Attribute.String & Schema.Attribute.Required;
+    linkedin: Schema.Attribute.String & Schema.Attribute.Required;
+    name: Schema.Attribute.String & Schema.Attribute.Required;
+    profile_photo: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios'
+    >;
+    role: Schema.Attribute.String & Schema.Attribute.Required;
+    tags: Schema.Attribute.JSON & Schema.Attribute.Required;
+    twitter: Schema.Attribute.String;
   };
 }
 
@@ -44,13 +89,31 @@ export interface SharedSocialLink extends Struct.ComponentSchema {
   };
 }
 
+export interface SharedWorkflowSteps extends Struct.ComponentSchema {
+  collectionName: 'components_shared_workflow_steps';
+  info: {
+    displayName: 'Workflow steps';
+  };
+  attributes: {
+    description: Schema.Attribute.String & Schema.Attribute.Required;
+    icon: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'> &
+      Schema.Attribute.Required;
+    num: Schema.Attribute.String & Schema.Attribute.Required;
+    title: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
       'content.faq-item': ContentFaqItem;
+      'content.milestones': ContentMilestones;
+      'content.stats': ContentStats;
       'shared.difference-point': SharedDifferencePoint;
+      'shared.founders': SharedFounders;
       'shared.navigation-link': SharedNavigationLink;
       'shared.social-link': SharedSocialLink;
+      'shared.workflow-steps': SharedWorkflowSteps;
     }
   }
 }
